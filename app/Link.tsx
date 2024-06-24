f31ba3978e67d633fb8f2f766ef542f0bb832c89
zhemu <lingx@ibuscloud.com>
2024-06-24 14:47:55 +0800

    fear: 搞出个导航栏

---
+++ app/Link.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { MouseEvent } from "react";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import { useRouter } from "next/navigation";
+import { FC, useTransition } from "react";
+interface ILinkProps {
+  className?: string;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  href: string;
+  target?: string;
+}
+
+function isModifiedEvent(event: MouseEvent<HTMLAnchorElement>): boolean {
+  const eventTarget = event.currentTarget;
+  const target = eventTarget.getAttribute("target");
+  return (
+    (target && target !== "_self") ||
+    event.metaKey ||
+    event.ctrlKey ||
+    event.shiftKey ||
+    event.altKey ||
+    (event.nativeEvent && event.nativeEvent.which === 2)
+  );
+}
+
+export default function Link({
+  className,
+  children,
+  style,
+  href,
+  target,
+  ...rest
+}: ILinkProps) {
+  const router = useRouter();
+  const [isNavigating, trackNavigation] = useTransition();
+  if (!target && !href.startsWith("/")) {
+    target = "_blank";
+  }
+  return <NextLink
+    {...rest}
+    target={target}
+    href={href}
+    onClick={(e) => {
+      if (!isModifiedEvent(e)) {
+        e.preventDefault()
+        trackNavigation(() => {
+          router.push(e.currentTarget.href)
+        })
+      }
+    }}
+    className={[className, `scale-100 active:scale-100`].join(" ")}
+    style={{
+      ...style,
+      transform: isNavigating ? "scale(1)" : "",
+      opacity: isNavigating ? 0.85 : 1,
+      transition: "transform 0.2s ease-in-out, opacity 0.2s 0.4s linear",
+    }}
+  >{children} </NextLink>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import AutoRefresh from './AutoRefresh';
 import { serif } from './fonts';
+import HomeLink from './HomeLink';
+import Link from './Link';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,7 +13,15 @@ export default function RootLayout({
       <html lang="en" className={serif.className}>
         <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
           <header className="mb-14 flex flex-row place-content-between">
-            <span className="relative top-[4px] italic">by </span>
+            <HomeLink />
+            <span className="relative top-[4px] italic ">by
+              <Link href="https://www.yuque.com/zhemu-emxtq" target="_blank" className='ml-2'>
+                <img
+                  alt="Zhe Mu"
+                  src="https://avatars.githubusercontent.com/u/82502913"
+                  className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
+                />
+              </Link></span>
           </header>
           <main>{children}</main>
         </body>
